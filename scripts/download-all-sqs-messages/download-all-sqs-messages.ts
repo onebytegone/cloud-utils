56058e29b6bfec446ab99ae8b7e5fff574ad8ff6
Ethan Smith <ethan@onebytegone.com>
2023-11-17 10:56:18 -0500

    feat: move default output to gitignored folder

--- scripts/download-all-sqs-messages/download-all-sqs-messages.ts
+++ scripts/download-all-sqs-messages/download-all-sqs-messages.ts
@@ -3,16 +3,20 @@ import { getRequiredArg } from '../../lib/minimist/get-required-arg';
 import { DeleteMessageBatchCommand, GetQueueUrlCommand, ReceiveMessageCommand, SQSClient } from '@aws-sdk/client-sqs';
 import { createWriteStream } from 'fs';
 import { DateTime } from 'luxon';
+import { dirname, join } from 'path';
+import { mkdir } from 'fs/promises';
 
 const sqs = new SQSClient({}),
       argv = minimist(process.argv, { string: [ 'queue-name', 'output' ], boolean: [ 'dry-run', 'delete' ] }),
       queueName = getRequiredArg(argv, 'queue-name'),
-      outputFile = argv.output ? argv.output : `${queueName}-${DateTime.utc().toFormat('yyyy-LL-dd\'T\'HHmmss')}.json`;
+      outputFile = argv.output ? argv.output : join('output', `${queueName}-${DateTime.utc().toFormat('yyyy-LL-dd\'T\'HHmmss')}.json`);
 
 (async () => {
    const queueURL = (await sqs.send(new GetQueueUrlCommand({ QueueName: queueName }))).QueueUrl;
 
    console.info(`Downloading messages from: ${queueURL} to ${outputFile}`);
+
+   await mkdir(dirname(outputFile), { recursive: true });
 
    const writeStream = createWriteStream(outputFile, 'utf-8');
 
